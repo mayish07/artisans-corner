672eeb891b766cd244ac201595d0dce94ec80189
Developer <developer@artisans-corner.com>
2026-04-26 11:13:36 +0100

    fix: remove redux-persist to fix error

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,45 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
 import authReducer from '../features/authSlice';
 import cartReducer from '../features/cartSlice';
-import productReducer from '../features/productSlice';
-import orderReducer from '../features/orderSlice';
-import uiReducer from '../features/uiSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-    cart: persistReducer(
-      {
-        key: 'cart',
-        version: 1,
-        storage,
-      },
-      cartReducer
-    ),
-    products: productReducer,
-    orders: orderReducer,
-    ui: uiReducer,
+    cart: cartReducer,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
-  devTools: process.env.NODE_ENV !== 'production',
 });
-
-export const persistor = persistStore(store);
 
 export default store;
