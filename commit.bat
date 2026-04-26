fae862b5639a26fdda14558b790ed23aefa17f8c
Developer <developer@artisans-corner.com>
2026-04-26 13:02:07 +0100

    Cleanup: Remove unnecessary project documentation files

--- commit.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-"C:\Program Files\Git\cmd\git.exe" add -A
-"C:\Program Files\Git\cmd\git.exe" commit -m "Fix server for Vercel deployment"
-"C:\Program Files\Git\cmd\git.exe" push origin main
